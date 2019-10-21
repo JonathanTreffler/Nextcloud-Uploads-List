01beddf812cea38e36146c2c7ccc963e83705673
Jonathan Treffler <trefflerj@web.de>
2019-10-21 23:54:16 +0200

    removed temporary debugging console output

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
     $(window).load(function() {
         let otherNavbarMenues = ["#contactsmenu",".notifications"];
         let uploads = [];
-        
+
         function createGUI(){
             let headerRight = $("#header > div.header-right");
-            
+
             let uploadsmenu = document.createElement("div");
             uploadsmenu.id="uploadsmenu";
-            
+
             let menutoggle = document.createElement("div");
             menutoggle.classList.add("icon-uploadsmenu");
             menutoggle.classList.add("menutoggle");
@@ -18,22 +18,20 @@
             $(menutoggle).attr("role","button");
             $(menutoggle).attr("aria-haspopup",true);
             $(menutoggle).css("background-image","var(--icon-upload-fff)");
-            
+
             let menu = document.createElement("div");
             menu.id = "uploadsmenu-menu";
             menu.classList.add("menu");
-            
+
             let uploadsDiv = document.createElement("div");
-            
+
             menu.appendChild(uploadsDiv);
             uploadsmenu.appendChild(menutoggle);
             uploadsmenu.appendChild(menu);
             headerRight.prepend(uploadsmenu);
-            
-            console.log(uploadsmenu);
         }
-        
-        
+
+
         function closeOtherNavbarMenues(){
            for(let navbarMenueId in otherNavbarMenues){
                $(otherNavbarMenues[navbarMenueId]+" > div.menu").css("display","none");
@@ -52,25 +50,23 @@
         function closeUploadsMenu(){
             $("#uploadsmenu > div.menu").css("display","none");
         }
-        
+
         function configureEventListeners(){
             $("#uploadsmenu > div.menutoggle")[0].addEventListener("click",function(){
                 closeOtherNavbarMenues();
                 setUploadsMenuState($("#uploadsmenu > div.menu").css("display") == "none");
             });
-            
+
             for(let navbarMenueId in otherNavbarMenues){
-                console.log( $(otherNavbarMenues[navbarMenueId]+" > div.menutoggle"));
-                
                 $(otherNavbarMenues[navbarMenueId]+" > div.menutoggle")[0].addEventListener("click",function(){
                     closeUploadsMenu();
                 });
             }
         }
-        
+
         function checkForChanges(){
             uploads = [];
-            
+
             for(let uploadId in FileList._uploader._uploads){
                 let upload = FileList._uploader._uploads[uploadId];
                 uploads.push({
@@ -83,33 +79,32 @@
                     bitrate: upload.data.progress().bitrate,
                 });
             }
-            
-            console.log(uploads);
+
             updateGUI();
         }
-        
+
         function updateGUI(){
             $("#uploadsmenu-menu > div").empty();
-            
+
             if(uploads.length == 0){
                 $("#uploadsmenu-menu > div").text("Derzeit keine Uploads");
             }else{
                 for(let uploadId in uploads){
                     let upload = uploads[uploadId];
-                    
+
                     let uploadDiv = document.createElement("div");
                     $(uploadDiv).css("width","100%");
-                    
+
                     let nameP = document.createElement("p");
                     $(nameP).text(upload.name);
                     $(nameP).css("background-color","gray");
-                    
+
                     let progressP = document.createElement("p");
                     $(progressP).text(upload.progress*100+" %");
-                    
+
                     let sizeP = document.createElement("p");
                     $(sizeP).text(upload.totalSize);
-                    
+
                     uploadDiv.appendChild(nameP);
                     uploadDiv.appendChild(progressP);
                     uploadDiv.appendChild(sizeP);
@@ -117,11 +112,11 @@
                 }
             }
         }
-        
-        
+
+
         createGUI();
         configureEventListeners();
-        
+
         setInterval(checkForChanges,1000);
     });
 })(window, jQuery);
